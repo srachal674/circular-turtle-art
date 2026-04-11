4570e7a7f893e09d5ea11305955878cc5bfe8efb
Leigh Rachal <srachal674@gmail.com>
2026-04-10 18:21:31 -0700

    Refactor draw_shape function to use wavelength instead of frequency for improved clarity

--- circular_generative_graphic.py
+++ circular_generative_graphic.py
@@ -10,6 +10,7 @@ t.pensize(50)
 screen = turtle.Screen()
 screen.bgcolor("#000066")
 screen.tracer(0, 0)
+phase = 25
 
 # Create a full-spectrum HCL rainbow palette (256 shades)
 palette = rainbow_hcl(c=250, l=50)
@@ -18,15 +19,16 @@ color_range = palette.colors(256)
 def draw_shape():
     x_start = -200
     x_end = 200
-    frequency = 40
+    wavelength = 40
     amplitude = 50
+    
 
     t.penup()
-    t.goto(x_start, math.sin(x_start / frequency) * amplitude)
+    t.goto(x_start, math.sin(x_start / wavelength) * amplitude)
     t.pendown()
 
     for x in range(x_start, x_end + 1):
-        y = math.sin(x / frequency) * amplitude
+        y = math.sin((x / wavelength) + phase) * amplitude
         color_index = int((x - x_start) / (x_end - x_start) * (len(color_range) - 1))
         t.pencolor(color_range[color_index])
         t.goto(x, y)
